f87ca40b6d48dbc298032f71a0147f5e51c3f140
gabrielgallo18 <96590617+gabrielgallo18@users.noreply.github.com>
2023-01-10 19:53:58 -0300

    Add files via upload

---
+++ objeto_JS/objeto.js
@@ -0,0 +1,18 @@
+// let idade = 5;
+// console.log(idade);
+// let altura = 188;
+// console.log(altura);
+
+let nome = 'Gabriel';
+let idade = '20';
+let estaAprovado = true;
+let corSelecionada = null;
+
+let pessoa = {
+    nome: 'Gabriel',
+    idade: 20,
+    estaAprovado: true,
+    sobrenome: 'Gallo',
+}
+
+console.log(pessoa)
